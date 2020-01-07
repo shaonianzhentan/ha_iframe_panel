1fd5fd837a6426171cd763e1b4b0c7a30e18a22c
zhulu <shaonianzhentan@sina.cn>
2020-01-07 16:24:08 +0800

    加入在线tab模式

--- custom_components/ha_iframe_panel/__init__.py
+++ custom_components/ha_iframe_panel/__init__.py
@@ -1,8 +1,13 @@
 import os
+import yaml
+import uuid
+from aiohttp import web
+from homeassistant.components.http import HomeAssistantView
 
 DOMAIN = 'ha_iframe_panel'
-VERSION = '1.3'
-ROOT_PATH = '/ha_iframe_panel-api/' + VERSION
+VERSION = '1.4'
+URL = '/ha_iframe_panel-api-' + str(uuid.uuid4())
+ROOT_PATH = '/ha_iframe_panel-local/' + VERSION
 
 def setup(hass, config):
     cfg  = config[DOMAIN]
@@ -14,6 +19,7 @@ def setup(hass, config):
     hass.components.frontend.add_extra_js_url(hass, ROOT_PATH + '/ha-iframe-panel.js')
     for i, item in enumerate(cfg):
         _dict = {}
+        _panel = "ha-iframe-panel"
         frontend_url_path = 'ha_iframe_panel_'+str(i)
         if item['type'] == 'fullscreen':
             _dict['fullscreen'] = True
@@ -30,6 +36,10 @@ def setup(hass, config):
         elif item['type'] == 'tabs':
             _dict['list'] = item['list']
             _name = 'TAB页面'
+        elif item['type'] == 'online':            
+            _dict['url'] = ROOT_PATH + '/index.html?api=' + URL
+            _panel = "iframe"
+            _name = '在线TAB页面'
         
         if 'name' in item:
             _name = item['name']
@@ -39,11 +49,37 @@ def setup(hass, config):
             _icon = item['icon']
         
         hass.components.frontend.async_register_built_in_panel(
-            "ha-iframe-panel",
+            _panel,
             _name,
             _icon,
             frontend_url_path,
             config=_dict,
-            require_admin=False)
+            require_admin=True)
 
     return True
+
+
+class HassGateView(HomeAssistantView):
+
+    url = URL
+    name = DOMAIN
+    requires_auth = False
+        
+    async def post(self, request):
+        hass = request.app["hass"]
+        try:
+            filename = hass.config.path("configuration.yaml")
+            config = yaml.full_load(get_file_content(filename))
+            cfg  = config[DOMAIN]
+            _list = []
+            for item in enumerate(cfg):
+                if item['type'] == 'online':
+                    _list.extend(item['list'])
+            return self.json({'code':0, 'msg': '查询成功', 'data': _list})
+        except Exception as e:
+            print(e)
+            return self.json({'code':1, 'msg': '出现异常'})
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
